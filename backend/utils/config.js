a2b35bf72f9187111b544c0db0f8cf48c11e4dd7
Ravindhar K <ravindharrv23@gmail.com>
2025-03-30 19:41:58 +0530

    removed unwanteed files

--- backend/utils/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export default  {
-    port: process.env.PORT || 3000,
-    uri: process.env.MONGO_URI,
-    secret_key:process.env.SECRET_KEY,
-    url: process.env.URL
-}
