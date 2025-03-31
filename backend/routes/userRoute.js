982c14dea4c6efea356eb4c8418b9bca85f6c500
Himalaya.tech <himalaya.jhala.31.3@moutblue.tech>
2025-03-31 11:51:16 +0530

    Adding routes

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,40 @@
+import express from "express";
+import {
+  followUser,
+  getUserDetails,
+  getUserDetailsById,
+  loginUser,
+  logoutUser,
+  searchUsers,
+  signupUser,
+  updatePassword,
+  updateProfile,
+} from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import { uploadAvatar } from "../utils/awsFunctions.js";
+
+const router = express.Router();
+
+// multer instance
+const avatarUploader = uploadAvatar();
+
+router.route("/signup").post(avatarUploader.single("avatar"), signupUser);
+router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
+
+router.route("/me").get(isAuthenticated, getUserDetails);
+
+router.route("/user/:username").get(isAuthenticated, getUserDetails);
+router.route("/userdetails/:id").get(isAuthenticated, getUserDetailsById);
+
+router
+  .route("/update/profile")
+  .put(isAuthenticated, avatarUploader.single("avatar"), updateProfile);
+router.route("/update/password").put(isAuthenticated, updatePassword);
+
+router.route("/follow/:id").get(isAuthenticated, followUser);
+
+router.route("/users").get(isAuthenticated, searchUsers);
+router.route("/users/suggested").get(isAuthenticated, searchUsers);
+
+export default router;
